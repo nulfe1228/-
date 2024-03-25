18d3cd50c78a43accedd67b10b3e7267ed334509
nulfe1228 <160991121+nulfe1228@users.noreply.github.com>
2024-03-25 17:02:12 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 
+const imgList = document.getElementById('lookbook_list')
+
+// 이미지 리스트
+for (let i = 0; i < 65; i++) {
+    // Image 객체 생성
+    const img = document.createElement('img');
+    // src 속성에 파일 주소 지정
+    img.src = `images/model-${i+1}.jpg`;
+    // 요소에 삽입
+    imgList.appendChild(img);
+}
