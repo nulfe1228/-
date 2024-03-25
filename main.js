abc66080968ea36ebb71f84e8a9db6381d28f898
nulfe1228 <160991121+nulfe1228@users.noreply.github.com>
2024-03-25 17:37:31 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-
 const imgList = document.getElementById('lookbook_list')
+
 
 // 이미지 리스트
 for (let i = 0; i < 65; i++) {
@@ -9,4 +9,15 @@ for (let i = 0; i < 65; i++) {
     img.src = `images/model-${i+1}.jpg`;
     // 요소에 삽입
     imgList.appendChild(img);
+}
+
+
+
+// 페이지 이동
+function shopPage(){
+    window.location.href = 'https://www.michaelkors.com/michael-kors-collection.html'
+}
+
+function lastSeasonPage(){
+    window.location.href = 'https://www.michaelkors-collection.com/runway/ss24-michael-kors-collection'
 }
